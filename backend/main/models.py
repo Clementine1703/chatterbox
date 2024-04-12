ca52b2bfc46e4b640af41cf3364e58b4e55cab9c
Kamil Alimetov <kamil.alimetov@gmail.com>
2024-04-12 16:26:02 +0300

    chat realization

--- backend/main/models.py
+++ backend/main/models.py
@@ -3,35 +3,39 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    # first_name = models.CharField(max_length=0)
-    # last_name = models.CharField(max_length=0)
     pass
 
-class UserAdditionalData(models.Model):
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    status = models.CharField(max_length=30)
-    bio = models.TextField(max_length=500, blank=True)
-    sex = models.CharField(max_length=30)
-    birth_date = models.DateField(null=True, blank=True)
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    # Дополнительные поля профиля пользователя
 
-class Friendship(models.Model):
-    pass
-
-class FriendRequest(models.Model):
-    pass
 
 class Chat(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    # Дополнительные поля чата
+
+class ChatParticipant(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    # Дополнительные поля участника чата
 
 class Message(models.Model):
-    pass
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    text = models.TextField()
+    # Дополнительные поля сообщения
 
 class Community(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    # Дополнительные поля сообщества
 
 class CommunityMember(models.Model):
-    pass
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+    # Дополнительные поля участника сообщества
 
-class CommunityPublications(models.Model):
-    pass
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(CustomUser, related_name='sent_friend_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(CustomUser, related_name='received_friend_requests', on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+    # Дополнительные поля заявки в друзья
--- frontend/src/components/UserChats.vue
+++ frontend/src/components/UserChats.vue
@@ -1,6 +1,190 @@
 <template>
-  <q-page class="flex flex-center">
-    <p>CHATS</p>
+  <q-page class="flex flex-center kaka">
+    <div class="wrapper">
+      <div class="chat-list">
+        <div>
+    <q-list bordered padding>
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced />
+      <q-item-label header>List Header</q-item-label>
+
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+
+    </q-list>
+  </div>
+      </div>
+      <div class="active-chat"></div>
+    </div>
   </q-page>
 </template>
 
@@ -11,3 +195,24 @@ export default defineComponent({
   name: 'UserChats'
 })
 </script>
+
+<style scoped lang="scss">
+  .wrapper{
+    display: flex;
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  .chat-list{
+    width: 30%;
+    height: 70vh;
+    overflow-y: auto;
+  }
+
+  .active-chat{
+    width: 70%;
+    height: 70vh;
+    background-color: red;
+  }
+</style>
